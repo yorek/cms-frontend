176d1ee2dca348a39922f9085d700a021a32b562
Davide Mauri <damauri@microsoft.com>
2022-08-18 13:52:06 -0700

    updated link

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ export default function Footer({ }) {
     return (
         <footer className={styles.footer}>
             <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                href="#"
                 target="_blank"
                 rel="noopener noreferrer"
             >
